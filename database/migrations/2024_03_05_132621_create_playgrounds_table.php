a2441a33086e6aff3aca688e3914978db638dddd
Thomayor <tmayor@laigledu.net>
2024-03-06 09:22:59 +0100

    add info for playground

--- database/migrations/2024_03_05_132621_create_playgrounds_table.php
+++ database/migrations/2024_03_05_132621_create_playgrounds_table.php
@@ -18,8 +18,15 @@ class CreatePlaygroundsTable extends Migration
             $table->string('name', 50)->unique();
             $table->string('surface_type', 20);
             $table->boolean('is_covered');
+            $table->boolean('is_booked');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
+            $table->string('city', 20)->nullable();
+            $table->string('adress', 150)->nullable();
+            $table->string('postcode', 20)->nullable();
+            $table->float('coordgpsx', 10, 6);
+            $table->float('coordgpsy', 10, 6);
+            
         });
     }
 
